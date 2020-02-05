76fc12a572a0a1332ca9e99c523177569fab4c00
dgarciaap <dgarcia24@ucol.mx>
2020-02-05 12:50:06 -0600

    Add methods to search for gifs

--- lib/gifs_diana.rb
+++ lib/gifs_diana.rb
@@ -1,6 +1,26 @@
 require "gifs_diana/version"
+require "httparty"
 
 module GifsDiana
   class Error < StandardError; end
-  # Your code goes here...
+  #makes consuming restful web services dead easy.
+  include HTTParty
+
+  base_uri "animatedgif.me"
+  
+  #finding by id
+  def self.find_by_id(id)
+	  get("/gifs/#{id}.json").parsed_response["url"]
+  end
+
+  #finding by tag
+  def self.find_by_tag(tag)
+	  get("/#{tag}.json").parsed_response["url"]
+  end
+
+  #find a random kitty
+  def self.random(tag)
+	  find_by_tag(tag)
+  end
+  
 end
