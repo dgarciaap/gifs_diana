af016b4c098632c44740bb338d2e1134d55fac07
dgarciaap <dgarcia24@ucol.mx>
2020-02-06 11:00:02 -0600

    Comment test

--- test/gifs_diana_test.rb
+++ test/gifs_diana_test.rb
@@ -4,8 +4,13 @@ class GifsDianaTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::GifsDiana::VERSION
   end
-
+  
   def test_it_does_something_useful
-    assert false
+	  assert true
   end
+
+  #def test_success_status_code
+    #response = GifsDiana.find_by_id(368)  
+    #assert_equal "#<Net::HTTPOK 200 OK readbody=true>", response.response
+  #end
 end
