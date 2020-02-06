3fbb3789f7f8eea322abffdaf1985f1a88239493
dgarciaap <dgarcia24@ucol.mx>
2020-02-06 10:41:26 -0600

    Generate a txt file with the url retrieved

--- lib/gifs_diana.rb
+++ lib/gifs_diana.rb
@@ -7,20 +7,35 @@ module GifsDiana
   include HTTParty
 
   base_uri "animatedgif.me"
+
+
+  def self.generate_txt(petition)
+	  begin
+		file = File.new("gifs_urls", "w")
+		file.puts petition
+		puts "Txt generated :)"
+		puts "Route = #{file.path}"
+		file.close
+	  rescue Exception, "No jaló"
+	  end
+  end
   
   #finding by id
   def self.find_by_id(id)
-	  get("/gifs/#{id}.json").parsed_response["url"]
+	  gifs_urls = get("/gifs/#{id}.json").parsed_response["url"]
+	  self.generate_txt(gifs_urls)
   end
 
   #finding by tag
   def self.find_by_tag(tag)
-	  get("/#{tag}.json").parsed_response["url"]
+	  gifs_urls = get("/#{tag}.json").parsed_response["url"]
+	  generate_txt(gifs_urls)
   end
 
   #find a random kitty
   def self.random(tag)
-	  find_by_tag(tag)
+	  gifs_url = find_by_tag(tag)
+	  generate_txt(gifs_url)
   end
-  
+
 end
